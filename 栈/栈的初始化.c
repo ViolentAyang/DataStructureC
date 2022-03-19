a31492b8d188f0d7a88313c6a19af59866b0d69f
ViolentAyang <76544389+ViolentAyang@users.noreply.github.com>
2022-03-19 21:04:22 +0800

    Create 栈的初始化.c

---
+++ 栈/栈的初始化.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <stdbool.h>
+#define MaxSize 10
+
+typedef struct{
+	int data[MaxSize];  //静态数组存放栈中元素
+	int top;  //栈顶指针
+}SqStack;
+
+//初始化栈
+void InitStack(SqStack *S){
+	S->top = -1;
+}
+//判断栈空
+bool StackEmpty(SqStack s){
+	if(S.top==-1){
+		return false;
+	}else{
+		return true;
+	}
+}
+int main(){
+	SqStack S;
+	InitStack(&S);
+	return 0;
+}
